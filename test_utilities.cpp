400ffe71579eafceb390a27c90835a95b9561fc8
=Fatima Ahmad <=fahmad.bsai24seecs@seecs.edu.pk>
2025-09-13 16:23:51 +0500

    added a subtracting function

--- test_utilities.cpp
+++ test_utilities.cpp
@@ -8,5 +8,6 @@ int main() {
     int num1;//prompt user to enter 2 numbers
     int num2;
     cout << "adding("<<num1<<","<<num2<<")==5: " << (add(num1,num2) == 5 ? "PASS" : "FAIL") << endl;
+    cout<<"Subtracting ("<<num1<<","<<num2<<")==5:"<<(subtract(num1,num2) == 5 ? "PASS" : "FAIL") << endl;//new function
     return 0;
 }
--- utilities.h
+++ utilities.h
@@ -3,6 +3,6 @@
 
 
 int add(int a, int b);  // placeholder example
-
+int  subtract(int a,int b);//new function declared
 
 #endif
